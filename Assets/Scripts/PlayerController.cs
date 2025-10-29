b8e5f291ded92a62050ec569da2dd0a33353718a
Maarten Kramer <86170398+git85740@users.noreply.github.com>
2025-10-29 17:28:04 +0100

    Animation and run implementation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,34 +1,66 @@
 using UnityEngine;
 using System.Collections;
-
 public class PlayerController : MonoBehaviour
 {
     public float speed = 5f;
     private Rigidbody rb;
     private Vector3 moveInput;
-
+    private Animator anim;
+    private bool isRunning = false;
     private void Start()
     {
+        anim = GetComponentInChildren<Animator>();
         rb = GetComponent<Rigidbody>();
         rb.interpolation = RigidbodyInterpolation.None;
-
         StartCoroutine(EnableInterpolationAfterDelay(0.5f));
     }
-
-    //enables interpolation after delay
+    // enables interpolation after delay
     private IEnumerator EnableInterpolationAfterDelay(float delay)
     {
         yield return new WaitForSeconds(delay);
         rb.interpolation = RigidbodyInterpolation.Interpolate;
     }
-
     private void Update()
     {
+
+        // checks if player is running
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            speed = 8;
+            isRunning = true;
+        }
+        else
+        {
+            speed = 5;
+            isRunning = false;
+        }
+
+        // calculates movement
         float moveX = Input.GetAxisRaw("Horizontal");
         float moveZ = Input.GetAxisRaw("Vertical");
         moveInput = new Vector3(moveX, 0, moveZ).normalized * speed;
-    }
 
+        // animation and character direction logic
+        if (moveX != 0 || moveZ != 0)
+        {
+            anim.SetBool("isWalk", true);
+            anim.SetBool("isRun", isRunning);
+
+            if (moveX < 0)
+            {
+                anim.transform.localEulerAngles = new Vector3(0, 180, 0);
+            }
+            else if (moveX > 0)
+            {
+                anim.transform.localEulerAngles = new Vector3(0, 0, 0);
+            }
+        }
+        else
+        {
+            anim.SetBool("isWalk", false);
+            anim.SetBool("isRun", false);
+        }
+    }
     private void FixedUpdate()
     {
         if (moveInput.magnitude > 0.1f)
