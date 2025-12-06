81a32c139d1bf5ea347124604c19ac144a809ae4
Maarten Kramer <86170398+git85740@users.noreply.github.com>
2025-12-07 00:04:54 +0100

    Busywork Quest added
    added a basic busywork quest for the testday including audio.

---
+++ Assets/Scripts/Interaction.cs
@@ -0,0 +1,64 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.Audio;
+
+public class Interaction : MonoBehaviour
+{
+    public GameObject printer;
+    public GameObject desk;
+    public int printedItems = 0;
+    private bool playerInside = false;
+    private bool interactionCooldown = false;
+    private string currentObject = "";
+
+    public AudioSource printerAudio;
+    public AudioSource deskAudio;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Printer"))
+        {
+            currentObject = "Printer";
+            playerInside = true;
+        }
+
+        if (other.CompareTag("Desk"))
+        {
+            currentObject = "Desk";
+            playerInside = true;
+        }
+    }
+
+    void Update()
+    {
+        if (playerInside && Input.GetKeyDown(KeyCode.E))
+        {
+            if (currentObject == "Printer" && interactionCooldown == false && printedItems < 5)
+            {
+                StartCoroutine(Cooldown());
+                printedItems++;
+                
+            }
+            if (currentObject == "Desk" && interactionCooldown == false)
+            {
+                printedItems = 0;
+                //desk.SetActive(false);
+                deskAudio.Play();
+            }
+        }
+
+        if (printedItems == 5)
+        {
+            desk.SetActive(true);
+        }
+    }
+
+    IEnumerator Cooldown()
+    {
+        interactionCooldown = true;
+        printerAudio.Play();
+        yield return new WaitForSeconds(3f);
+        interactionCooldown = false;
+        Debug.Log(printedItems);
+    }
+}
