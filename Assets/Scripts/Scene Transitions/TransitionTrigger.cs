4cc371eecb73caa0f8648b4bf36e001cff91c05d
Loonmos <demi.marsman@gmail.com>
2026-01-06 21:09:59 +0100

    finished the cutscenes and prologue parts are connected

--- Assets/Scripts/Scene Transitions/TransitionTrigger.cs
+++ Assets/Scripts/Scene Transitions/TransitionTrigger.cs
@@ -5,6 +5,7 @@ public class TransitionTrigger : MonoBehaviour
     public string sceneToLoad;
     public string exitPointID;
 
+    public bool autoTransition;
     private bool inRange;
 
     //Sends relevant data to scene fader
@@ -27,6 +28,11 @@ public class TransitionTrigger : MonoBehaviour
     void Update()
     {
         if (inRange && Input.GetKeyDown(KeyCode.E) && SceneFader.Instance != null)
+        {
+            SceneFader.Instance.TransitionToScene(sceneToLoad, exitPointID);
+        }
+
+        if (inRange && autoTransition && SceneFader.Instance != null)
         {
             SceneFader.Instance.TransitionToScene(sceneToLoad, exitPointID);
         }
